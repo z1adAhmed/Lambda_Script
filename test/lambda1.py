8ffd13099d19f69132b1ef2a70c50d23b659a81d
Ziad Ahmed <ziad.a200343@gmail.com>
2025-12-10 10:47:56 +0200

    Testing the work flow2

--- test/lambda1.py
+++ test/lambda1.py
@@ -1,5 +1,5 @@
 def lambda_handler(event, context):
-    print("Hello World1")
+    print("Hello World_one")
     return {
         "statusCode": 200,
         "body": "Hello World"
--- test/lambda2.py
+++ test/lambda2.py
@@ -1,5 +1,5 @@
 def lambda_handler(event, context):
-    print("Hello World2")
+    print("Hello World_two")
     return {
         "statusCode": 200,
         "body": "Hello World"
