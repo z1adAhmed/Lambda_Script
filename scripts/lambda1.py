391e57c9dfb93b38518dd03c283ed76e5565645f
Ziad Ahmed <ziad.a200343@gmail.com>
2025-12-11 14:53:33 +0200

    Made this repo or prod

--- scripts/lambda1.py
+++ scripts/lambda1.py
@@ -1,5 +1,5 @@
 def lambda_handler(event, context):
-    print("sawy")
+    print("zoz")
     return {
         "statusCode": 200,
         "body": "Hello World"
--- test/lambda1.py
+++
@@ -1,6 +0,0 @@
-def lambda_handler(event, context):
-    print("Good")
-    return {
-        "statusCode": 200,
-        "body": "Hello World"
-    }
